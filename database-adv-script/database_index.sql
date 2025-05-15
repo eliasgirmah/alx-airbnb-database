e64826a09171cd658fd4978baceff8651e5c562f
Elias <eliasgirma.h@gmail.com>
2025-05-15 06:05:40 +0300

    index

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,13 +1,14 @@
--- Index on User ID (commonly used in joins)
-CREATE INDEX idx_user_user_id ON "User" (user_id);
+-- BEFORE adding index (simulated for documentation purposes)
+-- Run this before creating the index
+EXPLAIN ANALYZE SELECT * FROM Booking WHERE user_id = 5;
 
--- Index on Booking's user_id and property_id
+-- Create indexes
 CREATE INDEX idx_booking_user_id ON Booking (user_id);
 CREATE INDEX idx_booking_property_id ON Booking (property_id);
-
--- Index on Booking start_date for filtering/sorting
-CREATE INDEX idx_booking_start_date ON Booking (start_date);
-
--- Index on Property ID and name
 CREATE INDEX idx_property_property_id ON Property (property_id);
 CREATE INDEX idx_property_name ON Property (name);
+CREATE INDEX idx_booking_start_date ON Booking (start_date);
+CREATE INDEX idx_user_user_id ON "User" (user_id);
+
+-- AFTER adding index
+EXPLAIN ANALYZE SELECT * FROM Booking WHERE user_id = 5;
